21db8f0b580ec7c4f7b4159c4cbf11ad8a81d126
Ponkan2077 <138117500+Ponkan2077@users.noreply.github.com>
2024-09-19 20:52:12 +0800

    some changes

--- TaskSystem/pages/includes/aside.php
+++ TaskSystem/pages/includes/aside.php
@@ -1,19 +1,15 @@
-<?php 
-     $path = $_SERVER['DOCUMENT_ROOT'];
-?>
-
 
 <div class="asideWrapper">
         
     <div class="menuHome">
-        <a href="#">Menu</a>
+        <a href="/yara/TaskSystem/index.html">Menu</a>
     </div>
     <nav class="asideNav">
          <ul>
-            <li class="dashboard"><a data-active="home" href="#">DASHBOARD</a></li>
-            <li class="reports"><a  data-active ="report"   href="">REPORTS</a></li>
-            <li class="settings"><a data-active = "setting" href="./pages/setting.php">SETTINGS</a></li>
-            <li class="tasks"><a  data-active ="task" href="./pages/task.html">TASKS</a></li>
+            <li class="dashboard"><a data-active="home" href="/yara/TaskSystem/index.html">DASHBOARD</a></li>
+            <li class="reports"><a  data-active ="report"   href="/yara/TaskSystem/pages/report.php">REPORTS</a></li>
+            <li class="settings"><a data-active = "setting" href="/yara/TaskSystem/pages/setting.php">SETTINGS</a></li>
+            <li class="tasks"><a  data-active ="task" href="/yara/TaskSystem/pages/task.php">TASKS</a></li>
          </ul>
     </nav>
     <div class="logout">
--- TaskSystem/pages/report.php
+++ TaskSystem/pages/report.php
@@ -1,14 +1,34 @@
+<?php 
+      $path = $pathSave = $_SERVER['DOCUMENT_ROOT'];
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./assets/generalStyle.css">
+    <link rel="stylesheet" type="text/css" href="/yara/TaskSystem/assets/generalStyle.css">
+    <script src="https://kit.fontawesome.com/c0056d4561.js" crossorigin="anonymous"></script>
+
     <title>Document</title>
 </head>
 <body id="report">
-    <header></header>
+    <div class="gridWrapper">
+    <header>
+    <?php 
+         $path .= "/yara/TaskSystem/pages/includes/header.html";
+            include_once($path);
+         $path = $pathSave;
+        ?>
+    </header>
     <main><span>report</span></main>
-    <aside></aside>
+    <aside>
+    <?php 
+        $path .= "/yara/TaskSystem/pages/includes/aside.php";
+        include_once($path);
+        $path = $pathSave;
+        ?> 
+    </aside>
+    </div>
+    <script type="text/javascript"  src="/yara/TaskSystem/assets/script/script.js"></script>
 </body>
 </html>
