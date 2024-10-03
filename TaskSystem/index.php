9b789638be90cdc26412c104d0f5ab1bddc93637
Ponkan2077 <reyflores12380@gmail.com>
2024-10-03 15:13:59 +0800

    graphs

--- TaskSystem/index.php
+++ TaskSystem/index.php
@@ -23,6 +23,7 @@ if(isset($_SESSION['account'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/c0056d4561.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" type="text/css" href="./assets/generalStyle.css">
 
     <title>Document</title>
@@ -57,9 +58,53 @@ if(isset($_SESSION['account'])){
         </div>
     </header>
     <main>
-       <div class="mainWrapper">
-        <span></span>
+        <section class="dashMainSection">
+       <div class="dashMainTopWrapper">
+            <article class="dashLeftWrapper">
+                <div class="dashTaskWrapper">
+                <div class="labelTaskGraph">
+                <span>Tasks</span>
+                <div class="taskGraphWrapper">
+                    <canvas id="taskGraph"></canvas>
+                </div>
+                </div>
+                <div class="dashLeftBottomWrapper">
+                <div class="dashDeadlines">
+                <span>Upcoming Deadlines</span>
+                <div class="dashDead"></div>
+                </div>
+                <div class="dashRecent">
+                <span>Recent Activities</span>
+                <div class="dashAct"></div>
+                </div>
+                </div>
+                </div>
+            </article>
+            <article class="dashRightWrapper">
+                <div class="dashTopRightWrapper">
+                     <div class="dashPieGraph">
+                        <span>Task Distribution by Category</span>
+                        <div class="dashPieWrapper">
+                        <canvas class="dashPie" id="dashPie"></canvas>
+                        </div>
+                     </div>
+                     <div class="dashBarGraph">
+                        <span>Task Completion Overtime</span>
+                        <div class="dashBarWrapper">
+                        <canvas id="dashBar"></canvas>
+                        </div>
+                        
+                     </div>
+                </div>
+                <div class="dashBottomRightWrapper">
+                    <div class="dashLeaderboard">
+                        <span>Leaderboard</span>
+                        <div class="leaderboardWrapper"></div>
+                    </div>
+                </div>
+             </article>
        </div>
+       </section>
     </main>
     <aside>
         <div class="asideWrapper">
@@ -82,5 +127,81 @@ if(isset($_SESSION['account'])){
     </aside>
 </div>
 <script type="text/javascript"  src="./assets/script/script.js"></script>
+<script>
+    const ctx = document.getElementById('dashBar');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Day 1', 'Day 2', 'Day 3'],
+    datasets: [{
+      label: '# hours',
+      data: [12, 30,90],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+  }
+});
+
+    const task = document.getElementById('taskGraph');
+
+new Chart(task, {
+  type: 'doughnut',
+  data: {
+    labels: ['Completed Task', 'Pending Task', 'Overdue Task'],
+    datasets: [{
+      label: '# of Task',
+      data: [12, 30,90],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+  }
+});
+
+const taskCategory = document.getElementById('dashPie');
+
+new Chart(taskCategory, {
+  type: 'polarArea',
+  data: {
+    labels: ['Category 1', 'Category 2', 'Category 3'],
+    datasets: [{
+      label: '# of Task',
+      data: [12, 30,90],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+  }
+});
+</script>
 </body>
 </html>
